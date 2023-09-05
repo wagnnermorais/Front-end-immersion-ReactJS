a3c98b1a863106c7e31ae6ef3870c1e89af4025d
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-05 15:31:12 -0300

    feat: styling scoped component

---
+++ 05 - CSS no React/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import styles from "./Title.module.css";
+
+const Title = () => {
+  return (
+    <div>
+      <h2 className={styles.title}>Meu título!</h2>
+    </div>
+  );
+};
+
+export default Title;
