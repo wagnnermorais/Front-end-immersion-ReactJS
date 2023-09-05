f6d7cb59fcdb8b7830554143f17934d00ca606dc
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-05 14:28:42 -0300

    feat: using react fragments

---
+++ 04 - Avancando no React/src/components/Fragment.jsx
@@ -0,0 +1,14 @@
+const Fragment = () => {
+  return (
+    <>
+      <h2>
+        <p>Temos dois elementos pais.</p>
+      </h2>
+      <h2>
+        <p>Este também é um elemento pai.</p>
+      </h2>
+    </>
+  );
+};
+
+export default Fragment;
