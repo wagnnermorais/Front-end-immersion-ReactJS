b061c55b014cb6e25d5b1149faeb2b7eca0e140e
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-05 17:28:29 -0300

    feat: initial react project structure

---
+++ 06 - Formulários em React/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <h1>Formulários em React</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
