efda66e1791c970c41f35e9d4b0de4dc9b873355
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-05 15:30:39 -0300

    feat: component styling

---
+++ 05 - CSS no React/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1 className="title">CSS do componente</h1>
+      <p className="paragraph">Estilo do parágrafo do componente.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
