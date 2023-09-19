e1d5934393695fbe729402f8750ee9c0f30e2a92
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-19 19:39:58 -0300

    feat: creating table component and returning mocked data

--- React Projects/Calculadora IMC/components/Table.jsx
+++ React Projects/Calculadora IMC/components/Table.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import "../styles/Table.css";
+import Button from "./Button";
 
-const Table = () => {
-  return <div>Table</div>;
+const Table = ({ data, imc, info, infoClass, reset }) => {
+  return (
+    <div className="container">
+      <div className="results">
+        <h1>
+          Your BMI: <span className={infoClass}>{imc}</span>
+        </h1>
+      </div>
+      <div className="results">
+        <h2>
+          Current Situation: <span className={infoClass}>{info}</span>
+        </h2>
+      </div>
+      <h3 className="classification-title">Confira as classificações:</h3>
+      <div className="imc-table">
+        <div className="table-titles">
+          <h3>BMI</h3>
+          <h3>Classification</h3>
+          <h3>Obesity</h3>
+        </div>
+        {data.map((item) => (
+          <div className="imc-result-box" key={item.info}>
+            <p>{item.classification}</p>
+            <p>{item.info}</p>
+            <p>{item.obesity}</p>
+          </div>
+        ))}
+      </div>
+      <div className="button-box">
+        <Button text={"Back"} action={reset} />
+      </div>
+    </div>
+  );
 };
 
 export default Table;
