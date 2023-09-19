65599e10c7151d11b7514164d8af8919ddb836cd
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-18 23:16:29 -0300

    refac: testing different approach in state lift component

--- 04 - Avancando no React/src/components/ChangeMessage.jsx
+++ 04 - Avancando no React/src/components/ChangeMessage.jsx
@@ -1,10 +1,16 @@
+// eslint-disable-next-line react/prop-types
 const ChangeMessage = ({ handleMessage }) => {
   const messages = ["Oi", "Olá", "Tudo bem?"];
   return (
     <div>
-      <button onClick={() => handleMessage(messages[0])}>1</button>
+      {/* <button onClick={() => handleMessage(messages[0])}>1</button>
       <button onClick={() => handleMessage(messages[1])}>2</button>
-      <button onClick={() => handleMessage(messages[2])}>3</button>
+      <button onClick={() => handleMessage(messages[2])}>3</button> */}
+      {messages.map((message, index) => (
+        <button onClick={() => handleMessage(messages[index])} key={index}>
+          {index + 1}
+        </button>
+      ))}
     </div>
   );
 };
