373198c5e02b9cd5f662ff600d5dc8b3e74c8006
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-22 23:58:44 -0300

    feat: creating input component

---
+++ React Projects/Multi Step Form/src/components/Input.jsx
@@ -0,0 +1,21 @@
+import "../../styles/Components/Input.css";
+
+const Input = ({ title, type, name, placeholder, state, value }) => {
+  return (
+    <div>
+      <label>
+        <span className="label">{title}</span>
+        <input
+          className="input"
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          onChange={(e) => state(e)}
+          value={value}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default Input;
