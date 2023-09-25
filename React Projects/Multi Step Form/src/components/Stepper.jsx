68f98753227cafeededa832696df21ab676703a2
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-24 21:15:08 -0300

    feat: adding stepper logic and refactoring component style

--- React Projects/Multi Step Form/src/components/Stepper.jsx
+++ React Projects/Multi Step Form/src/components/Stepper.jsx
@@ -1,20 +1,19 @@
 import "../../styles/Components/Stepper.css";
+import { AiOutlineUser, AiOutlineStar, AiOutlineSend } from "react-icons/ai";
 
-const Stepper = () => {
+const Stepper = ({ currentStep }) => {
   return (
     <div className="stepper-container">
-      <div className="stepper-box">
-        <img src="/user.png" alt="" />
+      <div className="stepper-box active">
+        <AiOutlineUser />
         <p>Identificação</p>
       </div>
-      <div className="border"></div>
-      <div className="stepper-box">
-        <img src="/star.png" alt="" />
+      <div className={`stepper-box ${currentStep >= 1 ? "active" : ""}`}>
+        <AiOutlineStar />
         <p>Avaliação</p>
       </div>
-      <div className="border"></div>
-      <div className="stepper-box">
-        <img src="/send.png" alt="" />
+      <div className={`stepper-box ${currentStep >= 2 ? "active" : ""}`}>
+        <AiOutlineSend />
         <p>Envio</p>
       </div>
     </div>
