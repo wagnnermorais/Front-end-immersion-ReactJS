531882630853cce2ec48e79e42c5b410299d5974
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-25 15:49:06 -0300

    refac: creating columns box

--- React Projects/Multi Step Form/src/components/Satisfaction.jsx
+++ React Projects/Multi Step Form/src/components/Satisfaction.jsx
@@ -8,69 +8,74 @@ import {
 
 const Satisfaction = ({ data, updateFieldHandler }) => {
   return (
-    <div className="satisfaction-container">
-      <div className="satisfaction-test">
+    <div className="satisfaction-section">
+      <div className="satisfaction-container">
         <div className="satisfaction-box">
-          <label className="radio-box">
-            <input
-              type="radio"
-              name="review"
-              value="unsatisfied"
-              checked={data.review === "unsatisfied"}
-              onChange={(e) => updateFieldHandler("review", e.target.value)}
-              required
-            />
-            <BsEmojiFrownFill />
-            <span>Insatisfeito</span>
-          </label>
+          <div className="column-box">
+            <label className="radio-box">
+              <input
+                type="radio"
+                name="review"
+                value="unsatisfied"
+                checked={data.review === "unsatisfied"}
+                onChange={(e) => updateFieldHandler("review", e.target.value)}
+                required
+              />
+              <BsEmojiFrownFill />
+              <span>Unsatisfied</span>
+            </label>
+          </div>
+          <div className="column-box">
+            <label className="radio-box">
+              <input
+                type="radio"
+                name="review"
+                value="neutral"
+                checked={data.review === "neutral"}
+                onChange={(e) => updateFieldHandler("review", e.target.value)}
+              />
+              <BsEmojiNeutralFill />
+              <span>Neutral</span>
+            </label>
+          </div>
         </div>
         <div className="satisfaction-box">
-          <label className="radio-box">
-            <input
-              type="radio"
-              name="review"
-              value="neutral"
-              checked={data.review === "neutral"}
-              onChange={(e) => updateFieldHandler("review", e.target.value)}
-            />
-            <BsEmojiNeutralFill />
-            <span>Poderia ser melhor</span>
-          </label>
+          <div className="column-box">
+            <label className="radio-box">
+              <input
+                type="radio"
+                name="review"
+                value="satisfied"
+                checked={data.review === "satisfied"}
+                onChange={(e) => updateFieldHandler("review", e.target.value)}
+              />
+              <BsEmojiSmileFill />
+              <span>Satisfied</span>
+            </label>
+          </div>
+          <div className="column-box">
+            <label className="radio-box">
+              <input
+                type="radio"
+                name="review"
+                value="delighted"
+                checked={data.review === "delighted"}
+                onChange={(e) => updateFieldHandler("review", e.target.value)}
+              />
+              <BsEmojiHeartEyesFill />
+              <span>Delighted</span>
+            </label>
+          </div>
         </div>
-        <div className="satisfaction-box">
-          <label className="radio-box">
-            <input
-              type="radio"
-              name="review"
-              value="satisfied"
-              checked={data.review === "satisfied"}
-              onChange={(e) => updateFieldHandler("review", e.target.value)}
-            />
-            <BsEmojiSmileFill />
-            <span>Satisfeito</span>
-          </label>
-        </div>
-        <div className="satisfaction-box">
-          <label className="radio-box">
-            <input
-              type="radio"
-              name="review"
-              value="delighted"
-              checked={data.review === "delighted"}
-              onChange={(e) => updateFieldHandler("review", e.target.value)}
-            />
-            <BsEmojiHeartEyesFill />
-            <span>Muito satisfeito</span>
-          </label>
-        </div>
+        <div className="satisfaction-box"></div>
       </div>
       <div className="text-area-box">
         <label htmlFor="comment" className="comment">
-          Comentário:
+          Comment:
         </label>
         <textarea
           name="comment"
-          placeholder="Conte como foi sua experiência com o produto"
+          placeholder="Tell us about your experience with the product."
           rows="5"
           value={data.comment}
           onChange={(e) => updateFieldHandler("comment", e.target.value)}
