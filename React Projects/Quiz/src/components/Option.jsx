7df6e07d5ea887765fc16f9153fa411007077a38
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-21 19:43:09 -0300

    feat: creating options component

---
+++ React Projects/Quiz/src/components/Option.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+import "../styles/Option.css";
+
+const Option = ({ option, selectOption, answer }) => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div
+      className={`options ${
+        quizState.answerSelected && option === answer ? "correct" : ""
+      } ${quizState.answerSelected && option !== answer ? "wrong" : ""}`}
+      onClick={() => selectOption()}
+    >
+      <p>{option}</p>
+    </div>
+  );
+};
+
+export default Option;
