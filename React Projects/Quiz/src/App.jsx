cf7eb49475da091f474f0a195ffffdab0b4884d6
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-11-21 19:42:38 -0300

    feat: adding game steps

--- React Projects/Quiz/src/App.jsx
+++ React Projects/Quiz/src/App.jsx
@@ -1,11 +1,21 @@
-import "../src/styles/App.css";
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+import "../src/styles/App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Start" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
