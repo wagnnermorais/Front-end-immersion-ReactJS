7ad2d5354b16baa3d26c5cf0b8d29099195b197b
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-10 20:48:20 -0300

    refac: removing components to use in routes

--- React Projects/Countdown/src/App.jsx
+++ React Projects/Countdown/src/App.jsx
@@ -1,24 +1,15 @@
-import Title from "./components/Title";
-import Counter from "./components/Counter";
+import { Outlet } from "react-router-dom";
 import background from "./assets/main-background.jpg";
 import styles from "./styles/App.module.css";
-import useCountdown from "./hooks/useCountdown";
 
 function App() {
-  const [days, hours, minutes, seconds] = useCountdown("Jan 1, 2024 00:00:00");
   return (
     <div
       className={styles.app}
       style={{ backgroundImage: `url(${background}` }}
     >
       <div className={styles.container}>
-        <Title title="Contagem regressiva para 2024" />
-        <div className={styles.countDownContainer}>
-          <Counter title={"Dias"} number={days} />
-          <Counter title={"Horas"} number={hours} />
-          <Counter title={"Minutos"} number={minutes} />
-          <Counter title={"Segundos"} number={seconds} />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
