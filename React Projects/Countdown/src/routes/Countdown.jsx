2fa3262d6ad32a3fd4b5c23318cbccd2857619ad
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-11 23:51:20 -0300

    feat: adding context in countdown component

--- React Projects/Countdown/src/routes/Countdown.jsx
+++ React Projects/Countdown/src/routes/Countdown.jsx
@@ -1,13 +1,24 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import Title from "../components/Title";
 import Counter from "../components/Counter";
 import useCountdown from "../hooks/useCountdown";
+import { CountDownContext } from "../context/CountDownContenxt";
 import styles from "../styles/Countdown.module.css";
 
 const Countdown = () => {
-  const [days, hours, minutes, seconds] = useCountdown();
+  const { event } = useContext(CountDownContext);
+  const [days, hours, minutes, seconds] = useCountdown(event.date);
+  const eventTitle = event.title;
+  const eventColor = event.color;
+
+  if (!event) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
-      <Title title="Contagem regressiva para 2024" />
+      <Title title={eventTitle} />
       <div className={styles.countDownContainer}>
         <Counter title={"Dias"} number={days} />
         <Counter title={"Horas"} number={hours} />
