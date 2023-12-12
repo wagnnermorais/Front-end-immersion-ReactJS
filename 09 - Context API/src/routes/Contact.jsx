4f7cd21f542f7a75964f024a648614c97ae2e836
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-12 19:03:41 -0300

    feat: using button to increment number by context

--- 09 - Context API/src/routes/Contact.jsx
+++ 09 - Context API/src/routes/Contact.jsx
@@ -1,7 +1,13 @@
+// 3 - alterando valor do contexto
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+
 const Contact = () => {
+  const { counter } = useContext(CounterContext);
   return (
     <div>
       <h1>Contact</h1>
+      <p>Valor do contador: {counter}</p>
     </div>
   );
 };
--- 09 - Context API/src/routes/Home.jsx
+++ 09 - Context API/src/routes/Home.jsx
@@ -1,7 +1,15 @@
+// 3 - alterando valor do contexto
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
+
 const Home = () => {
+  const { counter } = useContext(CounterContext);
   return (
     <div>
       <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
     </div>
   );
 };
