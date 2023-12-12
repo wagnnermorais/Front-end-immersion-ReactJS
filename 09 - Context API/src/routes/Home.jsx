a0bb0f4a6729b0bc4e65837abad471f9557e1f46
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-12 19:44:41 -0300

    feat: adding custom hook to use context

--- 09 - Context API/src/routes/Home.jsx
+++ 09 - Context API/src/routes/Home.jsx
@@ -1,10 +1,15 @@
 // 3 - alterando valor do contexto
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
 import ChangeCounter from "../components/ChangeCounter";
 
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // 3 - alterando valor do contexto
+  // const { counter } = useContext(CounterContext);
+  // 4 - refatorando com hook
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>Home</h1>
