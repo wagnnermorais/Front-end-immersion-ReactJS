04375adb205430016cad1e1cfcfbce378dd3097c
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-14 18:54:06 -0300

    feat: creating project structure

--- 10 - React hooks/src/main.jsx
+++ 10 - React hooks/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./routes/Home.jsx";
+import Contact from "./routes/Contact.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
