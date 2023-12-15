990aeb4f95c3b0794e840713c067b732c806b075
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-14 22:27:59 -0300

    feat: adding navigation to main component

--- 10 - React hooks/src/App.jsx
+++ 10 - React hooks/src/App.jsx
@@ -1,9 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
+      <ul
+        style={{
+          display: "flex",
+          gap: "1rem",
+          justifyContent: "space-between",
+          padding: 0,
+        }}
+      >
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contato</Link>
+        </li>
+      </ul>
       <Outlet />
     </>
   );
