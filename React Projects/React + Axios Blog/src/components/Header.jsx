93156b93877282e2aee045a8ba322591cc0e47cb
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-17 22:03:47 -0300

    feat: creating header component

---
+++ React Projects/React + Axios Blog/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <Navbar />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
